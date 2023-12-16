25e01673eef7d840c9bd74b9725ac412920c7064
AnswerShy <qqeanswershy@gmail.com>
2023-12-16 16:13:19 +0100

    multiplayerAdded
    there are branch, where multiplayer was added. Multiplayer was made with socket.io

--- public/scripts/NoEngineScripts/leaderboard.js
+++ public/scripts/NoEngineScripts/leaderboard.js
@@ -2,7 +2,6 @@ $(document).ready(() => {
     var json = 'scripts/NoEngineScripts/leaderboard.json'
     $.getJSON(json, function(data){
         for(let i = 0; i < data.length; i++){
-            console.log(data[i])
             document.getElementById("leaderboard").innerHTML += `${data[i].nickname}: ${data[i].wins}`
         }
     })
--- public/scripts/NoEngineScripts/mainMenu.js
+++ public/scripts/NoEngineScripts/mainMenu.js
@@ -8,7 +8,65 @@ document.querySelector(`#backButtonLB`).addEventListener("click", () => {
 
 document.querySelector("[play=\"bot\"]").addEventListener("click", () => {
     document.querySelector(".madalM").style.display = "none"
+    reFiller()
     isGameWBotStart()
     stepOfGame = 0
     gameMode = "bot"
+})
+
+//muliplayer buttons on menu
+document.querySelector("[play=\"human\"]").addEventListener("click", () => {
+    gameMode = "human"
+    document.querySelector(".mMultiplayer").style.display = "grid"
+    document.querySelector(".mMenu").style.display = "none"
+    reFiller()
+})
+
+document.querySelector(`#backButtonMulti`).addEventListener("click", () => {
+    document.querySelector(".mMenu").style.display = "grid"
+    document.querySelector(".mMultiplayer").style.display = "none"
+})
+
+let curId;
+let curCode;
+
+document.querySelector("#multiplayerNext").addEventListener("click", () => {
+    if(getUserNickname()) {
+        curId = generateRandomId()
+        document.querySelector("#lobbyCode").innerHTML = curId
+        socket.emit(`getNick`, p_nickname)
+        createLobby(curId)
+        joinLobby(curId)
+        document.querySelector(".mLobbySearch").style.display = "grid"
+        document.querySelector(`.mMultiplayer`).style.display = "none"
+    }
+    else {
+        document.querySelector("#wrongNick").style.display = "grid"
+    }
+})
+
+document.querySelector("#enterToLobbyAccept").addEventListener("click", () => {
+    curCode = document.querySelector("#enterToLobby").value
+    if(curCode == ""){
+        alert("Invalid lobby code")
+    }
+    else {
+        joinLobby(curCode)
+        console.log(curCode)
+    }
+})
+
+document.querySelector("#backToLobbySearch").addEventListener("click", () => {
+    socket.emit(`disonnect`, curCode)
+    curId = generateRandomId()
+    document.querySelector("#lobbyCode").innerHTML = curId
+    socket.emit(`getNick`, p_nickname)
+    createLobby(curId)
+    joinLobby(curId)
+    document.querySelector("#startLobbyGame").style.display = "grid"
+    document.querySelector(".mLobbySearch").style.display = "grid"
+})
+
+document.querySelector("#startLobbyGame").addEventListener("click", () => {
+    socket.emit("gameStart", curId)
 })
